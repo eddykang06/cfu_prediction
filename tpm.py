514279037a8742aa2da5d003fd5f826ef54b03f0
eddykang06 <eddykang06@gmail.com>
2026-04-13 10:10:46 -0400

    updated pipeline

--- tpm.py
+++ tpm.py
@@ -5,7 +5,6 @@
 #      
 # Note that the input folder should be the default
 
-
 # Packages
 import pandas as pd
 import numpy as np
@@ -18,32 +17,78 @@ fcnts_path = sys.argv[0] # folder
 cfu_path   = sys.argv[1]
 outfig     = sys.argv[2]
 
-# Function to 
-
-# can use df.rename(lambda x: ) to rename the samples?
-# Function to load the Fcnts data
-def extract_fcnts(folder_path):
+def read_fcnts(folder_path):
     '''
-    Input:
+    Extracts all Fcnt csvs from a given folder as a list of dataframes
+    Args:
         folder_path : file path containing Fcnts output from pipeline
-    Output - folder 
-        fcnt_dataframe_list : list of dataframes (converted from csvs)
+    Output: 
+        fcnt_df_list : list of Fcnts dataframes (converted from csvs)
     '''
 
     # Extract all file names within folder
-    files = os.listdir()
+    files = os.listdir(folder_path)
 
-    # Filter out any file names that have ".summary" in them
-    files = [csv in files if ".summary" not in csv]
+    # Filter out the summary files and keep only NDC comparisons
+    files = [csv for csv in files if ".summary" not in csv and "NDC0hr" in csv]
+
+    # Attach path to each file
+    filenames = ["".join([fcnts_path, "/" , csv]) for csv in files]
+
+    # Load each file as a dataframe
+    fcnt_df_list = [pd.read_table(csv, sep = "\t", header = 0, skiprows = 1) for csv in filenames]
+
+    return fcnt_df_list
+
+# Function to calculate TPMs from existing data
+def tpm_convert(fcnt_df_list):
+    '''
+    Converts a list of Fcnts dataframes to a single binded TPM dataframe
+    Input:
+        fcnt_df_list : 
+    Output:
+        all_tpm_df :
+
+    '''
+    for df in fcnt_df_list:
+
+        # Move gene names to index
+        df = df.set_index("Geneid")
+
+        # Remove all columns except for Length and Fcnts
+        df = df.loc[:, [col for col in list(df.columns) if col == "Length" or col.startswith("/")]]
+
+        # Convert all entries to integers
+        df = df.apply(lambda column: [int(entry) for entry in column])
+
+        # Convert gene length from b -> kb
+        df["Length"] = df["Length"].apply(lambda column: column / 1000)
+
+        # Select Fcnts columns and operate
+
+
+
+
+
+        # Remember to 
+
+
+        # 
+        all_tpm_df = ?
+    
+    return all_tpm_df
+
+# can use df.rename(lambda x: ) to rename the samples?
+
+
 
 # Function to calculate TPMs from
 
 
 # Function to load the CFU data
-def read_cfu(file_path):
+def read_cfus(file_path):
 
-# Function to convert Fcnt files to dataframes of TPMs
-def tpm_covert(fcnt_files):
+
 
 
 # Function to bind TPMs
@@ -51,3 +96,14 @@ def bind_data():
 
 # Call main
 def main(fcnts_path, cfu_path, outfig):
+
+    stored_fcnts = read_fcnts(fcnts_path)
+    stored_tpms  = tpm_convert(stored_fcnts)
+    stored_cfus  = read_cfus()
+    all_data     = bind_data(stored_fcnts, stored_cfus)
+    # 
+
+    return 
+
+if __name__ == "__main__":
+    main()
